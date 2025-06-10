a017162a1855eb5c449848c53e25437ff435f182
Narend <narendraghivari04@gmail.com>
2025-06-10 09:23:27 +0700

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-import { router } from './router.js';
-import 'leaflet/dist/leaflet.css';
-
-window.addEventListener('DOMContentLoaded', () => {
-  router();
-  window.addEventListener('hashchange', router);
-});
