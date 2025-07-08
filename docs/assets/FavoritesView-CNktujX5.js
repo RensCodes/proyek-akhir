11fa3525d5090bd0ed591a17040b501891e376c5
Narend <narendraghivari04@gmail.com>
2025-07-08 23:09:29 +0700

    Update FavoritesView-CNktujX5.js

--- docs/assets/FavoritesView-CNktujX5.js
+++ docs/assets/FavoritesView-CNktujX5.js
@@ -1,4 +1,4 @@
-import{U as e}from"./index-BKrFl1gL.js";const a={mainContent:null,presenter:null,setPresenter(n){this.presenter=n},renderLoading(){this.mainContent&&e.showLoadingSpinner(this.mainContent,"Memuat cerita favorit...")},renderStories(n,t="Pengguna"){if(this.mainContent){if(e.hideLoadingSpinner(this.mainContent),!n||n.length===0){this.mainContent.innerHTML=`
+import{U as e}from"./index-CiRWjb8D.js";const a={mainContent:null,presenter:null,setPresenter(n){this.presenter=n},renderLoading(){this.mainContent&&e.showLoadingSpinner(this.mainContent,"Memuat cerita favorit...")},renderStories(n,t="Pengguna"){if(this.mainContent){if(e.hideLoadingSpinner(this.mainContent),!n||n.length===0){this.mainContent.innerHTML=`
         <section class="page-section animate-fade-in">
           <h2>Halo, ${t}!</h2>
           <p>Kamu belum menyimpan cerita apa pun ke favorit.</p>
